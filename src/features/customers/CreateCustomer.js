1017a745ba4b4930559fcdb67e0d75f58eaf2544
Aniket Singh <thor98571@gmail.com>
2024-03-05 10:01:12 +0530

    completed redux bank with redux toolkit

--- src/features/customers/CreateCustomer.js
+++ src/features/customers/CreateCustomer.js
@@ -1,8 +1,9 @@
 import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { createCustomer} from "./customerSlice";
-
+import Web3 from 'web3';
 function Customer() {
+  const web3 = new Web3(window.ethereum);
   const [fullName, setFullName] = useState("");
   const [nationalId, setNationalId] = useState("");
   const dispatch=useDispatch()
@@ -10,6 +11,33 @@ function Customer() {
     if(!fullName||!nationalId)
       return
     dispatch(createCustomer(fullName,nationalId))
+  }
+  async function connect(){
+    // const isRabbyWalletInstalled = await web3.eth.getAccounts();
+    // console.log(isRabbyWalletInstalled)
+    // const rabbyWalletProvider = await web3.currentProvider.enable();
+    // console.log(rabbyWalletProvider)
+    if (window.rabby) {
+
+      try {
+        // Try to connect Rabby    
+        await window.rabby.enable() 
+        const rabbyAccounts = await web3.eth.getAccounts()
+        
+        // Rabby connected successfully
+        console.log('Rabby wallet connected', rabbyAccounts)
+  
+      } catch(err) {
+        // Handle error connecting Rabby
+        
+        console.log('Could not connect to Rabby', err)  
+      }
+  
+    } else {
+  
+      // Rabby not installed
+      console.log('Please install Rabby wallet') 
+    }
   }
 console.log("hey")
   return (
@@ -31,6 +59,7 @@ console.log("hey")
           />
         </div>
         <button onClick={handleClick}>Create new customer</button>
+        <button onClick={connect}>conect</button>
       </div>
     </div>
   );
